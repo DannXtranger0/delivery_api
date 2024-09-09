a797d8dc43daad0e870418ae22755583e0ad4c16
DannXtranger0 <robindannjf@gmail.com>
2024-09-09 10:19:14 -0500

    se crea el controlador LoginController y ahí se hacer el metodo login, en el cual recibe los datos del formulario y los valida, si está bien, crea un token y lo retorna, sino, dice que las credenciales son invalidas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Creamos un usuario por defecto
-        
+
         \App\Models\User::factory()->create([
             "name" => "Robin Jimenez",
             "email" => "robinjf@gmail.com",
             "password" => bcrypt("contrasena") //Hashea la contrasena
         ]);
-
-      
     }
 }
